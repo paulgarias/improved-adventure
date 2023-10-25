d7e413e32553adc5446ae368179237691e361be7
Paul G. Arias <paul.g.arias@gmail.com>
2023-10-25 12:14:01 -0400

    Update stats_file.py

--- stats_file.py
+++ stats_file.py
@@ -1,5 +1,11 @@
 import numpy as np
 def mean(alist):
+    """
+    This is a mean/average function
+    alist -> list type argument
+    examples:
+    >>> mean([2,3,4])
+    """
     alist = np.array(alist)
     tot_items = alist.shape[0]
     print(tot_items)
